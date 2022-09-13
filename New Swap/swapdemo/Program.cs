e94b8d6e09bcf251d715e542d75c7e5d9ab0fd23
Najmi <57437663+mnajmikl@users.noreply.github.com>
2022-09-14 01:07:52 +0800

    Add files via upload

---
+++ New Swap/swapdemo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+int[] myNum1 = new int[4] { 10, 20, 30, 40 };
+int[] myNum2 = new int[4] { 50, 60, 70, 80 };
+foreach (int i in myNum1)
+    Console.Write(i + " ");
+foreach (int i in myNum2)
+    Console.Write(i + " ");
+Console.WriteLine();
+(myNum2, myNum1) = (myNum1, myNum2);
+foreach (int i in myNum1)
+    Console.Write(i + " ");
+foreach (int i in myNum2)
+    Console.Write(i + " ");
